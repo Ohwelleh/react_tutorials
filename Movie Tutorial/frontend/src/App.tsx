0cc6f1b5d19a559b6b47d07cef879eedb56bb59d
Austin <austin7955@hotmail.com>
2025-05-22 16:20:43 -0700

    Implemented page routing with React-Router. Installed React-Router. Created a basic NavBar.

--- Movie Tutorial/frontend/src/App.tsx
+++ Movie Tutorial/frontend/src/App.tsx
@@ -1,12 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+
+// Pages and Components imports.
 import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import NavBar from "./components/NavBar";
+
 // Styling.
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
--- Movie Tutorial/frontend/src/pages/Home.tsx
+++ Movie Tutorial/frontend/src/pages/Home.tsx
@@ -1,12 +1,23 @@
+import { useState, type ChangeEvent, type FormEvent } from "react";
 import MovieCard from "../components/MovieCard";
+
+// Import types
 import type { Movie } from "../custom types/MovieTypes";
+
 function Home() {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
   const movies: Movie[] = [
     { id: 1, title: "John Wick", release_date: "2020" },
     { id: 2, title: "Terminator", release_date: "1999" },
     { id: 3, title: "The Matrix", release_date: "1998" },
   ];
-  function handleSearch() {}
+
+  function handleSearch(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    alert(searchQuery);
+  }
+
   return (
     <div className="home">
       <form onSubmit={handleSearch} className="search-form">
@@ -14,6 +25,10 @@ function Home() {
           type="text"
           placeholder="Search for movies..."
           className="search-input"
+          value={searchQuery}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setSearchQuery(e.target.value)
+          }
         />
         <button type="submit" className="search-btn">
           Search
