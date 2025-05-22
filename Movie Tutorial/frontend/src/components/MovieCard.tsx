d591c6d504b7edc4253fd8448dbc1ddda7b9fe90
Austin <austin7955@hotmail.com>
2025-05-22 11:52:50 -0700

    Implemented MovieCard and created a custom MovieType. Added folders to increase organization.

---
+++ Movie Tutorial/frontend/src/components/MovieCard.tsx
@@ -0,0 +1,25 @@
+import type { Movie } from "../custom types/MovieTypes";
+
+function MovieCard({ movie }: { movie: Movie }) {
+  function handleFavoriteClick() {
+    alert("Clicked");
+  }
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={movie.url} alt={movie.title} />
+        <div className="movie-overlay">
+          <button className="favorite-btn" onClick={handleFavoriteClick}>
+            💖
+          </button>
+        </div>
+      </div>
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date}</p>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
---
+++ Movie Tutorial/frontend/src/custom types/MovieTypes.tsx
@@ -0,0 +1,18 @@
+export type Movie = {
+  /**
+   * OPTIONAL: Unique Identifing number.
+   */
+  id?: number;
+  /**
+   * The full name of the movie.
+   */
+  title: string;
+  /**
+   * OPTIONAL: Path to an image of the movie.
+   */
+  url?: string;
+  /**
+   * The release date of the movie.
+   */
+  release_date: string;
+};
